6d26b5ed8d0b9a87e92292baf5c4dd0e31a4ebac
Andrew Anderson <AndrewPivots@GMail.com>
2019-02-01 22:06:02 -0700

    Delete Spec & Object Clean
    Added a new delete spec and cleaned up the object to practice. Reference lecturer a lot.

--- test-jasmine1/spec/AddressBookSpec.js
+++ test-jasmine1/spec/AddressBookSpec.js
@@ -1,11 +1,38 @@
-describe('AddressBook', function(){
-  it('should be able to add a contact', function(){
-      var addressBook = new AddressBook(),
-          contact = new Contact(),
-          thisContact = "Andrew";
+describe('AddressBook', function() {
 
-      addressBook.addContact(thisContact);
+  var addressBook,
+    contact,
 
-      expect(addressBook.getContact(0)).toBe(thisContact);
+  beforeEach(function() {
+    addressBook = new AddressBook();
+    contact = new Contact();
   });
-});
+
+  it('should be able to add a contact', function() {
+
+    addressBook.addContact('Joe');
+
+    expect(addressBook.getContact(0)).toBe('Joe');
+  });
+  it('should be able to delete a contact', function() {
+
+    addressBook.addContact('Joe');
+    addressBook.deleteContact(0);
+
+    expect(addressBook[0]).not.toBeDefined();
+  });
+}); // describe
+
+// BROKEN done() piece confusing
+// describe("Async Address Book", function() {
+//   var addressBook = new AddressBook();
+//   beforeEach(function(done) {
+//     addressBook.getInitialContacts(function() {
+//       done();
+//     });
+//   }); //beforeEach
+//   it('should grab initial contacts', function(done) {
+//     expect(addressBook.initialComplete).toBe(true);
+//     done();
+//   });
+// });
--- test-jasmine1/src/AddressBook.js
+++ test-jasmine1/src/AddressBook.js
@@ -1,9 +1,27 @@
 function AddressBook() {
-  let contacts = []
-  this.addContact = function(thisContact){
-    contacts.push(thisContact);
-  };
-  this.getContact = function(i){
-    return contacts[i];
-  }
+  let this.contacts = [];
+  // for async
+  // this.initialComplete = false;
+  //
+  // this.getInitialContacts = function(cb){
+  //   var self = this;
+  //   setTimeout(function(){
+  //     self.initialcomplete = true;
+  //     if (cb){
+  //       return cb();
+  //     }
+  //   }, 3);
+  // }
+}
+
+AddressBook.prototype.addContact = function(contact){
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.getContact = function(index){
+    return this.contacts[index];
+}
+
+AddressBook.prototype.deleteContact = function(index){
+  this.contacts.splice(index, 1);
 }
