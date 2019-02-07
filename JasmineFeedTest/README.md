55976b01deb9a6a9e968598f33dfb8d5908986a9
Andrew Anderson <AndrewPivots@GMail.com>
2019-02-06 22:59:36 -0700

    4th Submission
    Found toBeDefined function in Jasmine documentation. Fixed other things to be exactly inline with the unstructions.

--- JasmineFeedTest/README.md
+++ JasmineFeedTest/README.md
@@ -14,8 +14,10 @@ This project is to test my ability to add Jasmine js tests to an existing Udacit
 ## Projects Feedback
 1. FEB 4: My project reviewer thinks I over complicated most of my tests. This may have been partially because I wanted to stick to the methods I was most confident in at the time. I used a lot of loops to check more nodes which is a more thorough way of doing things but likely over-complicated it. But I did like implementing his method and techniques to make things simpler.
 2. FEB 5: Learned that you can put expect expressions within loops and even have multiple. Added missed expect statement. Fixed logic error that approved missing urls and names. Also, learned that these kind of test tools are for development branches and not for production so my UX concerns are not valid.
+3. FEB 6: Found toBeDefined function in Jasmine documentation. Fixed other things to be exactly inline with the unstructions.
 
 ## Sources
 1. CALLBACKS & NESTING: Karine L's thread in the Knowledge Center helped me understand nesting asynchronous functions with the callback. https://knowledge.udacity.com/questions/14179
 2. QUERY SELECTOR: My project reviewer recommends .querySelector over getElementsByClassName. Maybe because it's a more versatile method.
 3. CLASS LIST: My project reviewer recommends .classList of .className but I don't know why.
+4. TO BE DEFINED METHOD: https://jasmine.github.io/2.0/introduction.html
--- JasmineFeedTest/jasmine/spec/feedreader.js
+++ JasmineFeedTest/jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -36,12 +36,13 @@ $(function() {
            let i = 0;
            allFeeds.forEach(function(){
               expect(allFeeds[i].url.length).not.toBe(0);
+              expect(allFeeds[i].url).toBeDefined();
              i++;
            });//forEach
            });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -50,20 +51,19 @@ $(function() {
            let l = 0;
            allFeeds.forEach(function(){
              expect(allFeeds[l].name.length).not.toBe(0);
+             expect(allFeeds[l].name).toBeDefined();
              l++;
            });//forEach
            });
 
     });
 
-    /* TODO: Write a new test suite named "The menu" */
-
     describe('The menu', function(){
 
       const body = document.querySelector('body');
       const menuB = document.querySelector('.menu-icon-link');
 
-        /* TODO: Write a test that ensures the menu element is
+        /* a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -73,7 +73,7 @@ $(function() {
            expect(body.classList.contains('menu-hidden')).toBe(true); //improve: to check actual menu css
          });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -92,10 +92,7 @@ $(function() {
 
     }); // the menu
 
-
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+        /* a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -112,7 +109,7 @@ $(function() {
            });
            it('has at least one', function(){
              let entries = $('.feed .entry'); // reviewer told me to use ('.feed.entry') which didn't work. maybe I used it wrong.
-             expect(entries).not.toEqual(null);
+             expect(entries.length).not.toEqual(0); // null probably all for undefined etc
            });
          });
 
@@ -130,9 +127,7 @@ $(function() {
     //   });
     // });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
