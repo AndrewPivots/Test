70369013b2cb733b3aa2def56d56804bd53b6f4b
Andrew Anderson <AndrewPivots@GMail.com>
2019-03-10 22:15:19 -0600

    Quiz Default Function Parameters Start
    Udacity starter code. My answer in the next commit.

---
+++ functions1.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Using Default Function Parameters (2-2)
+ */
+
+// your code goes here
+
+/* tests
+console.log(buildHouse()); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({})); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({floors: 3, color: 'yellow'})); // Your house has 3 floor(s) with yellow brick walls.
+*/
