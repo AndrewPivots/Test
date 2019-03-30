a18508e6257861593beb58cc4a1a636febacca5d
Andrew Anderson <AndrewPivots@GMail.com>
2019-03-29 22:01:42 -0600

    Gulp Setup & Default Script 2
    Set up gulp on a project. Ran a default log.

--- gulp1/gulpfile.js
+++ gulp1/gulpfile.js
@@ -1,19 +1,7 @@
+// copied from Udacity
+
 const gulp = require("gulp");
-const sass = require("gulp-sass");
-const autoprefixer = require("gulp-autoprefixer");
 
-gulp.task("default", function() {
-  console.log("hello world!");
-});
-
-gulp.task("styles", function() {
-  gulp
-    .src("sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(
-      autoprefixer({
-        browsers: ["last 2 versions"]
-      })
-    )
-    .pipe(gulp.dest("./css"));
+gulp.task("default", function(){
+  console.log('Hello, gulp!');
 });
