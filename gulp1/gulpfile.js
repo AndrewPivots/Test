e5e50eaf1343b5df0571fea737d9902699b127a0
Andrew Anderson <AndrewPivots@GMail.com>
2019-04-02 22:09:33 -0600

    Sass w/ Gulp
    Setting up sass in a gulp environment and pushing a sass style. I didn't bother to include the node modules folder since it's so huge. Udacity tutorial.

--- gulp1/gulpfile.js
+++ gulp1/gulpfile.js
@@ -1,8 +1,18 @@
 // copied from Udacity
 
 const gulp = require("gulp");
+const sass = require("gulp-sass");
 
 gulp.task("default", function(done){
   console.log('Running gulp!');
+  done();
+});
+
+gulp.task("styles", function(done){
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(gulp.dest("./css"));
   done();
 });
